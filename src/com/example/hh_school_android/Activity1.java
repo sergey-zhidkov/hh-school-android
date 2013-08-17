e4c6f51ad38f9ab0fa0ffefc9e520b6f04964c4f
mobileDeveloper <4mobile.developer@gmail.com>
2013-08-17 19:19:07 +0400

    Initial commit

---
+++ src/com/example/hh_school_android/Activity1.java
@@ -0,0 +1,15 @@
+package com.example.hh_school_android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Activity1 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity1_linear);
+	}
+
+}
